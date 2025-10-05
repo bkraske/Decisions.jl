974073e79f253aabb5b12170d262ac83eef5d27e
bkraske <71412733+bkraske@users.noreply.github.com>
2025-10-04 22:44:44 -0600

    Use Dictionaries and some testing

--- test/momdp_pomdp_sandbox.jl
+++ test/momdp_pomdp_sandbox.jl
@@ -85,9 +85,9 @@ end
 function marginal_pdf(model,x,s,a,x_idx)
     supp = support(model[:sp];s=s,a=a)
     prob = 0.0
-    for s in supp
-        sprob = pdf(model[:sp],s;s=s,a=a)
-        if x == get_components(s,x_idx)
+    for sp in supp
+        sprob = pdf(model[:sp],sp;s=s,a=a)
+        if x == get_components(sp,x_idx)
             prob += sprob
         end
     end
@@ -98,8 +98,8 @@ function marginal_support(model,s,a,x_idx)
     supp = support(model[:sp];s=s,a=a)
     x = get_components(supp[1],x_idx)
     x_list = [x]
-    for s in supp[2:end]
-        x = get_components(s,x_idx)
+    for sp in supp[2:end]
+        x = get_components(sp,x_idx)
         inner_idx = findfirst(v->v==x,x_list)
         if isnothing(inner_idx)
             push!(x_list,x)
@@ -111,9 +111,9 @@ end
 function marginal_pdfy(model,yp,xp,s,a,y_idx,x_idx)
     supp = support(model[:sp];s=s,a=a)
     prob = 0.0
-    for s in supp
-        sprob = pdf(model[:sp],s;s=s,a=a)
-        if yp == get_components(s,y_idx) && xp == get_components(s,x_idx)
+    for sp in supp
+        sprob = pdf(model[:sp],sp;s=s,a=a)
+        if yp == get_components(sp,y_idx) && xp == get_components(sp,x_idx)
             prob += sprob
         end
     end
@@ -124,10 +124,10 @@ function marginal_supporty(model,xp,s,a,y_idx,x_idx)
     supp = support(model[:sp];s=s,a=a)
     yp = get_components(supp[1],y_idx)
     y_list = typeof(yp)[]
-    for s in supp
-        yp = get_components(s,y_idx)
+    for sp in supp
+        yp = get_components(sp,y_idx)
         inner_idx = findfirst(v->v==yp,y_list)
-        if isnothing(inner_idx) && get_components(s,x_idx) == xp
+        if isnothing(inner_idx) && get_components(sp,x_idx) == xp
             push!(y_list,yp)
         end
     end
@@ -148,106 +148,117 @@ function Decisions.transform(components::ToMixedObservability, model::POMDP_DN)
     sup_size = length(sup)
     x_type = typeof(get_components(sup[1],components.fully_observable_state_components))
     y_type = typeof(get_components(sup[1],components.partially_observable_state_components))
-    xs = x_type[] #(undef, sup_size)
-    ys = y_type[] #(undef, sup_size)
-    for s in sup
-        push!(xs,get_components(s,components.fully_observable_state_components))
-        push!(ys,get_components(s,components.partially_observable_state_components))
+
+    xs = Vector{x_type}(undef, sup_size)
+    ys = Vector{y_type}(undef, sup_size)
+    xys_dict = Dict{Tuple{x_type,y_type},typeof(sup[1])}()
+
+    for (i,s_i) in enumerate(sup)
+        x_i = get_components(s_i,components.fully_observable_state_components)
+        y_i = get_components(s_i,components.partially_observable_state_components)
+        xs[i] = x_i
+        ys[i] = y_i
+        push!(xys_dict,(x_i,y_i)=>s_i)
+        # x_i in xs ? nothing : push!(xs,x_i)
+        # y_i in ys ? nothing : push!(ys,y_i)
     end
+    xs = unique(xs)
+    ys = unique(ys)
+
     obs_type = typeof(support(model[:o])[1])
-    # mem_type = typeof(support(model[]))
 
     xtransition = @ConditionalDist x_type begin
         function support(;x,y,a)
             if isnothing(x) || isnothing(a) || isnothing(y)
                 xs
             else
-                s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
+                s = xys_dict[(x,y)]
+                # s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
                 marginal_support(model,s,a,components.fully_observable_state_components)
             end
         end
 
         function rand(rng;x,y,a) #rand! (?)
-            if POMDPs.isterminal(pomdp,s)
+            s = xys_dict[(x,y)]
+            if isterminal(s)
                 Terminal()
             else
-                s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-                get_components(model[:sp](;s=s,a=a),components.fully_observable_state_components)
+                get_components(model[:sp].rand(rng;s=s,a=a),components.fully_observable_state_components)
             end
         end
 
         function pdf(xp;x,y,a)
-            s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-            marginal_pdf(model,xp,s,a,x_idx)
+            s = xys_dict[(x,y)]
+            marginal_pdf(model,xp,s,a,components.fully_observable_state_components)
         end
     end
 
     ytransition = @ConditionalDist y_type begin
         function support(;x,y,a,xp)
             if isnothing(x) || isnothing(a) || isnothing(y) || isnothing(xp)
-                xs
+                ys
             else
-                s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
+                s = xys_dict[(x,y)]
                 marginal_supporty(model,xp,s,a,components.partially_observable_state_components,components.fully_observable_state_components)
             end
         end
 
         function rand(rng;x,y,a,xp) #rand! (?)
-            if POMDPs.isterminal(pomdp,s)
+            s = xys_dict[(x,y)]
+            if isterminal(s)
                 Terminal()
             else
-                s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
                 yp_support = marginal_supporty(model,xp,s,a,components.partially_observable_state_components,components.fully_observable_state_components)
-                yp_probs = [marginal_pdfy(model,yp,xp,s,a,components.partially_observable_state_components,components.fully_observable_state_components) for yp in support]
-                yp_support[rand(rng,DecisionDomains.Distributions.Categorical(yp_probs))]
+                yp_probs = [marginal_pdfy(model,yp,xp,s,a,components.partially_observable_state_components,components.fully_observable_state_components) for yp in yp_support]
+                yp_support[rand(rng,DecisionDomains.Distributions.Categorical(yp_probs))] #Make Cleaner
             end
         end
 
         function pdf(yp;x,y,a,xp)
-            s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
+            s = xys_dict[(x,y)]
             marginal_pdfy(model,yp,xp,s,a,components.partially_observable_state_components,components.fully_observable_state_components)
         end
     end
 
-    observation = @ConditionalDist obs_type begin
+    mom_observation = @ConditionalDist obs_type begin
         function support(;x,y,a,xp,yp)
             if isnothing(x) || isnothing(y) || isnothing(a) ||  isnothing(xp) || isnothing(yp)
                 support(model[:o])
             else
-                s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-                sp = s_from_xy(model,xp,yp,components.fully_observable_state_components,components.partially_observable_state_components)
-                Distributions.support(model[:o],s=s,a=a,sp=sp)
+                s = xys_dict[(x,y)]
+                sp = xys_dict[(xp,yp)]
+                support(model[:o];s=s,a=a,sp=sp)
             end
         end
 
         function rand(rng;x,y,a,xp,yp)
-            s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-            sp = s_from_xy(model,xp,yp,components.fully_observable_state_components,components.partially_observable_state_components)
-            model[:o](rng;s=s,a=a,sp=sp)
+            s = xys_dict[(x,y)]
+            sp = xys_dict[(xp,yp)]
+            model[:o].rand(rng;s=s,a=a,sp=sp)
         end
 
         function pdf(o;x,y,a,xp,yp)
-            s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-            sp = s_from_xy(model,xp,yp,components.fully_observable_state_components,components.partially_observable_state_components)
+            s = xys_dict[(x,y)]
+            sp = xys_dict[(xp,yp)]
             pdf(model[:o],o;s=s,a=a,sp=sp)
         end
     end
 
-    reward = @ConditionalDist Float64 begin
+    mom_reward = @ConditionalDist Float64 begin
         function rand(rng;x,y,a,xp,yp)
-            s = s_from_xy(model,x,y,components.fully_observable_state_components,components.partially_observable_state_components)
-            sp = s_from_xy(model,xp,yp,components.fully_observable_state_components,components.partially_observable_state_components)
-            model[:r](rng;a=a,s=s,sp=sp)
+            s = xys_dict[(x,y)]
+            sp = xys_dict[(xp,yp)]
+            model[:r].rand(rng;a=a,s=s,sp=sp)
         end
     end
 
-    belief = @ConditionalDist typeof(model[:mp]).parameters[2] begin #Return pomdp memory for now #FIX Typing here
+    mom_belief = @ConditionalDist typeof(model[:mp]).parameters[2] begin #Return pomdp memory for now #FIX Typing here
         function support(;a,m,o,x)
             support(model[:mp];a=a,m=m,o=o)
         end
 
         function rand(rng;a,m,o,x)
-            model[:mp](rng;a=a,m=m,o=o)
+            model[:mp].rand(rng;a=a,m=m,o=o)
         end
 
         function pdf(mp;a,m,o,x)
@@ -257,7 +268,30 @@ function Decisions.transform(components::ToMixedObservability, model::POMDP_DN)
 
     return MOMDP_DN(;xp = xtransition,
     yp = ytransition,
-    r = reward,
-    o = observation,
-    mp = belief)
+    r = mom_reward,
+    o = mom_observation,
+    mp = mom_belief,
+    a = model[:a])
 end
+
+#Test Sampling/Rand
+momdp[:xp](;x=rand(support(momdp[:xp])),a=rand(support(momdp[:a])),y=rand(support(momdp[:yp])))
+momdp[:yp](;x=rand(support(momdp[:xp])),a=rand(support(momdp[:a])),y=rand(support(momdp[:yp])))
+otest = momdp[:o](;x=rand(support(momdp[:xp])),y=rand(support(momdp[:yp])),a=rand(support(momdp[:a])),xp=rand(support(momdp[:xp])),yp=rand(support(momdp[:yp])))
+momdp[:r](;x=rand(support(momdp[:xp])),y=rand(support(momdp[:yp])),a=rand(support(momdp[:a])),xp=rand(support(momdp[:xp])),yp=rand(support(momdp[:yp])))
+momdp[:r](;x=([1,1],),y=([0,1,1],),a=1,xp=([1,1],),yp=([1,1,1],))
+btest = testrs.initial.rand()
+bp = momdp[:mp](;a=2,m=btest,o=3,x=rand(support(momdp[:xp]))) #Check belief update correct
+
+#support/pdf Test
+sp = support(momdp[:xp];x=([1, 1],), y= (Bool[0, 0, 0],),a=2)[1]
+pdf(momdp[:xp],sp;x=([1, 1],), y= (Bool[0, 0, 0],),a=2)
+sp = support(momdp[:yp];x=([1, 1],), y= (Bool[0, 0, 0],),a=2)[1]
+pdf(momdp[:yp],sp;x=([1, 1],), y= (Bool[0, 0, 0],),a=2)
+pdf(momdp[:yp],([0,0,0],);x=([1, 1],), y= (Bool[0, 0, 0],),a=2)
+o = support(momdp[:o];x=([1, 1],), y=(Bool[0, 0, 0],),a=1,xp=([1, 1],), yp=(Bool[0, 0, 0],))
+for obs in o
+    @show pdf(momdp[:o],obs;x=([1, 1],), y=(Bool[0, 0, 0],),a=1,xp=([1, 1],), yp=(Bool[0, 0, 0],))
+end
+support(momdp[:mp];a=2,m=btest,o=3,x=rand(support(momdp[:xp])))
+pdf(momdp[:mp],bp;a=2,m=btest,o=3,x=rand(support(momdp[:xp])))
